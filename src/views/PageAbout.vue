dc67cd2d980a42c385f9179acdc1f86673898854
jay3614 <wjdduddn12@naver.com>
2023-12-28 15:13:04 +0900

    커스텀 css, font 파일 추가

--- src/views/PageAbout.vue
+++ src/views/PageAbout.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="about">
-      <h1>This is an about page</h1>
-    </div>
-  </template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+.about {
+  min-height: 400px;
+}
+</style>
