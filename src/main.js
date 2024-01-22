ac96ce3a2a12ae8ecbd6cfbca1e99837432b2497
jay3614 <wjdduddn12@naver.com>
2024-01-22 16:58:25 +0900

    bootstrap 추가 및 myPage css 작업

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import axios from 'axios'
 import axios from './utils/axios'
 import router from './router'
 import store from './vuex/store'
@@ -8,6 +7,7 @@ import store from './vuex/store'
 import '../src/assets/css/main.css'
 import '../src/assets/css/util.css'
 import '../src/assets/css/myCustom.css'
+import '../src/assets/vendor/bootstrap/css/bootstrap.css'
 
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,25 +1,49 @@
 <template>
   <div class="myPage" id="wrapper">
     <nav class="mypage-sidenav">
-      <div class="test1">
+      <div class="my-activity">
         <h3>나의 활동</h3>
         <router-link to="#" class="d-flex p-l-10">메뉴1</router-link>
         <router-link to="#" class="d-flex p-l-10">메뉴2</router-link>
         <router-link to="#" class="d-flex p-l-10">메뉴3</router-link>
       </div>
-      <div class="test1">
+      <div class="my-info">
         <h3>내 정보</h3>
         <router-link to="#" class="d-flex p-l-10">메뉴1</router-link>
         <router-link to="#" class="d-flex p-l-10">메뉴2</router-link>
         <router-link to="#" class="d-flex p-l-10">메뉴3</router-link>
       </div>
     </nav>
-    <div class="mypage-cont">
-      <div class="test2">
+    <section class="mypage-cont my-info-modify">
+      <header class="info_views-area">
+        <div class="section-title">
+          <h1 class="tit">회원 정보 변경</h1>
+        </div>
+      </header>
 
+      <div class="table-responsive">
+        <table class="table">
+          <tbody>
+            <tr>
+              <th>아이디</th>
+              <td class="txt-left"><strong>[[${member.username}]]</strong></td>
+            </tr>
+
+            <tr>
+              <th>이름</th>
+              <td class="txt-left"><strong>[[${member.name}]]</strong></td>
+            </tr>
+
+            <tr id="email-area">
+              <th>이메일</th>
+              <td class="txt-left"><strong id="currentEmail"> [[${member.email}]]</strong>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </div>
-      </div>
+    </section>
+  </div>
 </template>
 
 <style>
